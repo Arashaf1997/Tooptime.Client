8cef41101440d868ebf2bbaeeea7fd12ca34295c
Mahdi-Fayyaz <118967843+Mahdi-Fayyaz@users.noreply.github.com>
2024-05-27 23:18:51 +0330

    next
    نصب Next و قرار دادن footer

--- app/src/components/footer/footer.jsx
+++ app/src/components/footer/footer.jsx
@@ -1,13 +1,11 @@
 import { Grid } from "@mui/material";
 import Details from "./details";
 import BottomFooter from "./bottomFooter";
-import TopFooter from "./topFooter";
 
 const Footer = () => {
   return (
     <>
       <Grid xs={12}>
-        <TopFooter/>
         <Details />
         <BottomFooter/>
       </Grid>
